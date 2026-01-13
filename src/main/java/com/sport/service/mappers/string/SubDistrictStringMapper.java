35d4c624aed8f964ba90a0629de258af7c41bf7a
Champion <assigov@mail.ru>
2026-01-13 19:27:47 +0300

    fixed SubDistrictStringMapper

--- src/main/java/com/sport/service/mappers/string/SubDistrictStringMapper.java
+++ src/main/java/com/sport/service/mappers/string/SubDistrictStringMapper.java
@@ -6,6 +6,9 @@ import com.sport.service.entities.enums.place.SubDistrict;
 public class SubDistrictStringMapper {
 
     public static String subDistrictEnumToSubDistrictString(SubDistrict subDistrict) {
+        if (subDistrict == null) {
+            return "";
+        }
         String subDistrictString;
         switch (subDistrict) {
             case SubDistrict.FROM_OSTUZHEVO_RING_TO_CHERNAVSKAAY_DAM ->
@@ -18,16 +21,14 @@ public class SubDistrictStringMapper {
             case SubDistrict.DIMITROVA_STREET -> subDistrictString = KeyboardConstants.DIMITROVA_STREET;
             case SubDistrict.FROM_CHERNAVSKYY_BRIDGE_TO_VOGRESOVSKYY_BRIDGE ->
                     subDistrictString = KeyboardConstants.FROM_CHERNAVSKYY_BRIDGE_TO_VOGRESOVSKYY_BRIDGE;
-            case SubDistrict.VAI_KRASNYY_OKTYABR ->
-                    subDistrictString = KeyboardConstants.VAI_KRASNYY_OKTYABR;
+            case SubDistrict.VAI_KRASNYY_OKTYABR -> subDistrictString = KeyboardConstants.VAI_KRASNYY_OKTYABR;
             case SubDistrict.PESCHANKA_OZERKI_SHINNIK_1_STARYY_MASHMET_BAM_MASHMET ->
                     subDistrictString = KeyboardConstants.PESCHANKA_OZERKI_SHINNIK_1_STARYY_MASHMET_BAM_MASHMET;
             case SubDistrict.FROM_NOVYY_BOMBEY_TO_ARMADA_TO_DEPUTATKA ->
                     subDistrictString = KeyboardConstants.FROM_NOVYY_BOMBEY_TO_ARMADA_TO_DEPUTATKA;
             case SubDistrict.PERVOE_MAAY_UGO_ZAPADNYY_ZAPADNYY_POSELOK ->
                     subDistrictString = KeyboardConstants.PERVOE_MAAY_UGO_ZAPADNYY_ZAPADNYY_POSELOK;
-            case SubDistrict.PRIDONSKOYY_PODKLETNOE ->
-                    subDistrictString = KeyboardConstants.PRIDONSKOYY_PODKLETNOE;
+            case SubDistrict.PRIDONSKOYY_PODKLETNOE -> subDistrictString = KeyboardConstants.PRIDONSKOYY_PODKLETNOE;
             case SubDistrict.TENNISTYY_ZAYYMISHE -> subDistrictString = KeyboardConstants.TENNISTYY_ZAYYMISHE;
             case SubDistrict.SHILOVO -> subDistrictString = KeyboardConstants.SHILOVO;
             case SubDistrict.REST_OF_CENTRALNYY_DISTRICT ->
@@ -38,7 +39,8 @@ public class SubDistrictStringMapper {
                     subDistrictString = KeyboardConstants.FROM_45_DIVISII_STREET_TO_9_YANVARY_STREET_AND_TO_MP;
             case SubDistrict.IPPODROM_FROM_MP_TO_ROTONDA_TO_URITSKOGO_STREET ->
                     subDistrictString = KeyboardConstants.IPPODROM_FROM_MP_TO_ROTONDA_TO_URITSKOGO_STREET;
-            case SubDistrict.PODGORNOE_HVOINYY_ZADONIE -> subDistrictString = KeyboardConstants.PODGORNOE_HVOINYY_ZADONIE;
+            case SubDistrict.PODGORNOE_HVOINYY_ZADONIE ->
+                    subDistrictString = KeyboardConstants.PODGORNOE_HVOINYY_ZADONIE;
             case SubDistrict.SEVERNYY -> subDistrictString = KeyboardConstants.SEVERNYY;
             default -> subDistrictString = KeyboardConstants.ALL_SUBDISTRICTS;
         }
@@ -58,8 +60,7 @@ public class SubDistrictStringMapper {
             case KeyboardConstants.DIMITROVA_STREET -> subDistrict = SubDistrict.DIMITROVA_STREET;
             case KeyboardConstants.FROM_CHERNAVSKYY_BRIDGE_TO_VOGRESOVSKYY_BRIDGE ->
                     subDistrict = SubDistrict.FROM_CHERNAVSKYY_BRIDGE_TO_VOGRESOVSKYY_BRIDGE;
-            case KeyboardConstants.VAI_KRASNYY_OKTYABR ->
-                    subDistrict = SubDistrict.VAI_KRASNYY_OKTYABR;
+            case KeyboardConstants.VAI_KRASNYY_OKTYABR -> subDistrict = SubDistrict.VAI_KRASNYY_OKTYABR;
             case KeyboardConstants.PESCHANKA_OZERKI_SHINNIK_1_STARYY_MASHMET_BAM_MASHMET ->
                     subDistrict = SubDistrict.PESCHANKA_OZERKI_SHINNIK_1_STARYY_MASHMET_BAM_MASHMET;
             case KeyboardConstants.FROM_NOVYY_BOMBEY_TO_ARMADA_TO_DEPUTATKA ->
