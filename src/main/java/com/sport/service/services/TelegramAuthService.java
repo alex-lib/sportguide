62c5313cc987db45866a9cf4272c6b0321b058c3
Champion <assigov@mail.ru>
2026-01-13 15:52:19 +0300

    commented signature in TelegramAuthService 2

--- src/main/java/com/sport/service/services/TelegramAuthService.java
+++ src/main/java/com/sport/service/services/TelegramAuthService.java
@@ -98,14 +98,14 @@ public class TelegramAuthService {
 
 
     private boolean validateTelegramData(Map<String, String> data) {
-        if (data.containsKey("signature")) {
-            log.info("Validating using signature (new Telegram WebApp)");
-            log.info("Data contains: {}", data.keySet());
-            log.info("Signature value: {}", data.get("signature"));
-            boolean result = validateSignature(data);
-            log.info("Signature validation result: {}", result);
-            return result;
-        }
+//        if (data.containsKey("signature")) {
+//            log.info("Validating using signature (new Telegram WebApp)");
+//            log.info("Data contains: {}", data.keySet());
+//            log.info("Signature value: {}", data.get("signature"));
+//            boolean result = validateSignature(data);
+//            log.info("Signature validation result: {}", result);
+//            return result;
+//        }
 
         if (data.containsKey("hash")) {
             log.info("Validating using hash (old Telegram WebApp)");
