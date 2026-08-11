6fb33726e5a5414312b496385a5974d4f19d3e79
aleksandr.sigov <aleksandr.sigov@redcollar.ru>
2026-08-11 15:36:30 +0300

    feat(SG-5): replaced criteria api for training program in query

--- src/main/java/com/sport/service/repositories/TrainingProgramRepository.java
+++ src/main/java/com/sport/service/repositories/TrainingProgramRepository.java
@@ -1,14 +1,25 @@
 package com.sport.service.repositories;
 
 import com.sport.service.entities.TrainingProgram;
+import com.sport.service.entities.enums.common.SportType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TrainingProgramRepository extends JpaRepository<TrainingProgram, Long>, JpaSpecificationExecutor<TrainingProgram> {
+public interface TrainingProgramRepository extends JpaRepository<TrainingProgram, Long> {
 
     Optional<TrainingProgram> findByTitle(String title);
+
+    @Query(nativeQuery = true, value = """
+            SELECT DISTINCT tp.* FROM training_programs tp
+            WHERE (:sportTypes::text[] IS NULL OR tp.sport_types && :sportTypes::text[])
+            """)
+    List<TrainingProgram> findWithFilters(
+            @Param("sportTypes") List<SportType> sportTypes
+    );
 }
