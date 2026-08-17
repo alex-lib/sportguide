efaf6b900b18a56f92a62dfd9620cf189b22c086
aleksandr.sigov <aleksandr.sigov@redcollar.ru>
2026-08-17 14:35:55 +0300

    feat(SG-11): fixed event filter

--- src/main/java/com/sport/service/repositories/EventRepository.java
+++ src/main/java/com/sport/service/repositories/EventRepository.java
@@ -21,14 +21,11 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Modifying
     void deleteByDateBefore(LocalDate currentDate);
 
-    @Query(nativeQuery = true, value = """ 
-            SELECT e.* FROM events e
-            WHERE (:district IS NULL OR e.district = :district) 
-              AND (:date::date IS NULL OR e.date = :date) 
-            ORDER BY e.date, e.time
-            """)
-    List<Event> findWithFilters(
-            @Param("district") District district,
-            @Param("date") LocalDate date
-    );
+    @Query("""
+    SELECT e FROM Event e
+    WHERE (:district IS NULL OR e.district = :district)
+      AND (:date IS NULL OR e.date = :date)
+    ORDER BY e.date, e.time
+    """)
+    List<Event> findWithFilters(@Param("district") District district, @Param("date") LocalDate date);
 }
